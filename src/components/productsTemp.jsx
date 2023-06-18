b8a82c4f12371a9d4b222dc9307d9ff2af870a66
your name <mohamedamin0az@gmail.com>
2023-06-18 17:06:28 +0300

    products styling inhancement

--- src/components/productsTemp.jsx
+++ src/components/productsTemp.jsx
@@ -4,28 +4,34 @@ import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 //styling
 import styles from "../styles/products.module.css";
+import { Grid } from "@mui/material";
 
 const ProductsTemp = ({ products }) => {
   return (
     <>
-      <div className={styles.container}>
+      <Grid container spacing={5} className={styles.container}>
         {products ? (
           products.map((item) => {
             return (
-              <div className={styles.productcontainer} key={item.id}>
-                <div className={styles.product}>
+                <Grid
+                  xs={6}
+                  sm={6}
+                  md={4}
+                  lg={3}
+                  item
+                  className={styles.product}
+                >
                   <NavLink to={`/product/${item.id}`}>
                     <h3>{item.title}</h3>
-                    <img src={item.image} />
+                    <img src={item.image} className="product-image" />
                   </NavLink>
-                </div>
-              </div>
+                </Grid>
             );
           })
         ) : (
           <h2>loading .....</h2>
         )}
-      </div>
+      </Grid>
     </>
   );
 };
